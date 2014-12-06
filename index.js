2c734cb09eca7be92bda85938c8c83ab97bf46dc
Daniel Mills <mills.dma@gmail.com>
2014-12-06 17:29:45 -0500

    added basic passport stuff

--- index.js
+++ index.js
@@ -1,66 +1,48 @@
-var http = require('http')
-var url = require('url')
 var fs = require('fs');
-var exec = require('child_process').exec;
-var jade = require('jade');
+var express = require('express');
+var session = require('express-session');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
-//load any extra variables to pass to jade
+//auth stuff
+var passport = require('./lib/passport').passport;
+var ensureAuthenticated = require('./lib/passport').ensureAuthenticated;
+
+// load config file
 var locals = require('./locals.json');
 
-var compile = jade.compileFile('views/content.jade',{
-	pretty:true
+var app = express();
+
+app.set('view engine', 'jade')
+app.set('views', __dirname + '/views')
+app.use(logger('dev'));
+app.use(bodyParser());
+app.use(cookieParser());
+app.use(session({
+	  secret: 'sowutidontcareatall'
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',ensureAuthenticated,function(req,res) {
+	res.status(200).render('content',locals);
 });
 
-if(process.argv.length < 3) {
-	console.log('USAGE: tailor <log file>');
-	process.exit(1);
-}
-
-var LOG_FILE = locals.filename = process.argv[2];
-
-if(!fs.existsSync(LOG_FILE)) {
-	console.log('Log file: ' + LOG_FILE + ' not found.');
-	process.exit(1);
-}
-
-//compile the client view
-var compiledView = compile(locals);
-
-var server = http.createServer(function(req,res) {
-
-	var uri = url.parse(req.url).pathname;
-
-	if(/.js$/.test(uri)) {
-		res.writeHead(200,{"Content-Type":"text/javascript"});
-		fs.createReadStream(uri.substr(1)).pipe(res);
-	} else if(uri === '/') {
-		res.writeHead(200,{"Content-Type":"text/html"});
-		res.end(compiledView);
-	} else if(/.css$/.test(uri)) {
-		res.writeHead(200,{"Content-Type":"text/css"});
-		fs.createReadStream(uri.substr(1)).pipe(res);
-	} else {
-		res.writeHead(400,{"Content-Type":"text/plain"});
-		res.end('404\n');
-	}
+app.get('/login',function(req,res) {
+	res.render('login',locals);
 });
 
-var io = require('socket.io')(server);
-
-//on a new connection get the last 20 lines and send to socket
-io.on('connection',function(socket) {
-	exec('tail -n 20 ' + LOG_FILE,function(err,stdout,stderr) {
-		if(err) throw err;
-		io.emit('init',stdout);
-	});
+app.get('/logout', function(req,res) {
+	req.logout();
+	res.redirect('/login');
 });
+app.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login', failureFlash: false }));
 
-//emit new file content to sockets
-var child = exec('tail -f ' + LOG_FILE);
-child.stdout.on('data',function(data) {
-	io.emit('message',data);
-});
+var server = app.listen(8000);
+var io = require('./lib/socket')(server,locals);
 
-server.listen(8000);
 io.listen(3000);
-console.log('server started on port 8000 watching log: ' + LOG_FILE);
+console.log('server started on port 8000 watching log: ' + locals.logfile);
